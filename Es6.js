e2bef4d19268e6f5643fc00884edf46193fc81c9
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-10-07 00:44:09 +0530

    set data structure::

--- Es6.js
+++ Es6.js
@@ -1,16 +1,16 @@
 // map data structure::
 
+// 1.
 let games = new Map();
 games.set('football', 'team');
 games.set('cricket', 'team');
 games.set('golf', 'individual');
 games.set('shooting', 'individual');
-
 console.log(games);
 
+// 2.
 let teamSport = ['football', 'polo', 'baseball'];
 let individualSport = ['shooting', 'chess', 'badminton'];
-
 let sports = new Map(
     [
         [teamSport, true],
@@ -18,3 +18,15 @@ let sports = new Map(
     ]
 );
 console.log(sports);
+
+
+// set data structure::
+
+let person = new Set(individualSport);
+console.log(person);
+
+person.add('chess');
+person.add('kabaddi');
+
+console.log(person); // removes duplicates
+person.clear();
