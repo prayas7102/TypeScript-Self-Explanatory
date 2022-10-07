66b1fd6374fec2f6ec126a09480852645ccfef57
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-10-07 09:36:30 +0530

    Js OOPs

--- Es6.js
+++ Es6.js
@@ -6,7 +6,7 @@ games.set('football', 'team');
 games.set('cricket', 'team');
 games.set('golf', 'individual');
 games.set('shooting', 'individual');
-console.log(games);
+// console.log(games);
 
 // 2.
 let teamSport = ['football', 'polo', 'baseball'];
@@ -14,19 +14,55 @@ let individualSport = ['shooting', 'chess', 'badminton'];
 let sports = new Map(
     [
         [teamSport, true],
-        [individualSport,'Italy']
+        [individualSport, 'Italy']
     ]
 );
-console.log(sports);
+// console.log(sports);
 
 
 // set data structure::
 
 let person = new Set(individualSport);
-console.log(person);
+// console.log(person);
 
 person.add('chess');
 person.add('kabaddi');
 
-console.log(person); // removes duplicates
+// removes duplicates
+// console.log(person);
 person.clear();
+
+// Destructuring
+let [first, , second] = ['CPP', 'Python', 'Java'];
+// console.log(first);
+// console.log(second);
+
+// OOPs
+
+function rectangle(width, length) {
+    this.width = width;
+    this.length = length;
+}
+let rect = new rectangle(20, 30);
+console.log(rect.length);
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+class Student extends Person {
+    constructor(name, age, school) {
+        super(name, age);
+        this.school = school;
+    }
+    getName() { return this.name; }
+    setName(name) { this.name = name; }
+}
+let xyz = new Person('rahul', 89);
+let pqr = new Student('abhay', 6, 'NCS');
+console.log(pqr);
+console.log(pqr.getName());
+pqr.setName('prayas');
+console.log(pqr.getName());
